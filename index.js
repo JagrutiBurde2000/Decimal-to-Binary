0e78bb5f62ae4025ad84ec1af03f7e378c971f73
JagrutiBurde2000 <130835386+JagrutiBurde2000@users.noreply.github.com>
2023-05-20 23:58:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+let decInput=document.getElementById("dec-input");
+let binInput=document.getElementById("bin-input");
+let errorMsg=document.getElementById("sample-error");
+
+decInput.addEventListener("input",()=>{
+    let decValue=parseInt(decInput.value)
+binInput.value=decValue.toString(2);
+  
+})
